6a1e3bbf789edd5f136b72fc89c3bea98c9c792d
Miles Roberts <milesrobertsmain@gmail.com>
2024-11-16 02:35:26 -0600

    added git functionality

--- main.sh
+++ main.sh
@@ -2,6 +2,9 @@
 mkdir -p ~/Documents/todo
 cd ~/Documents/todo
 
+# start git
+git init
+
 # initialize date to today
 date=$(date +%F)
 
@@ -59,5 +62,9 @@ while [[ $input != "q" ]]; do
             read input
             ;;
     esac
+
+    # git commit
+    git add $date
+    git commit -m "automated"
 done
 clear
